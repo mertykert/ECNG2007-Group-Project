365589fcb4c8a4551b7676122bfd3eb4d9711eed
Amrith Soochit <amrithsoochit25@gmail.com>
2025-10-18 18:11:55 -0400

    feat: HomeScreen
    style and feat: CalendarScreen
    
    style: ProfilesScreen
    
    feat: Firebase Connection - link accounts, store data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
